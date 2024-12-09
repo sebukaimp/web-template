61ba957dae42deca628749440cab51d521ae6a85
macarena <macarena.martinaracil@vivaticket.com>
2024-12-09 15:26:37 +0100

    roboto

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,18 +11,8 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxtjs/fontaine'
   ],
-  fontMetrics: {
-    fonts: ['Roboto'],
-  },
 
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
-        },
-      ],
-    },
-  },
+
+
+
 })
